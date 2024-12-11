29ea538bfcb1bf5ff11b8671eb68416e73ffe5af
abdullah <abdullahiqbal599@gmail.com>
2024-12-11 22:39:32 +0500

    upload data to neon

--- app/provider.js
+++ app/provider.js
@@ -1,6 +1,27 @@
-import React from 'react'
-
+'use client'
+import { db } from '@/config/db';
+import { useUser } from '@clerk/nextjs';
+import React, { useEffect } from 'react'
+import { users } from '@/config/schema'
+import { eq } from 'drizzle-orm';
 function Provider({ children }) {
+
+  const { user } = useUser()
+
+  useEffect(()=> {
+    user && newUser()
+  }, [user])
+  const newUser = async () => {
+    const finduser = await db.select().from(users).where(eq(users.email, user?.primaryEmailAddress.emailAddress))
+    console.log(finduser)
+    if(!finduser[0]){
+      const newUser = await db.insert(users).values({
+        name: user?.fullName,
+        email: user?.primaryEmailAddress.emailAddress,
+        imageUrl: user?.imageUrl
+      })
+    }
+  };
   return (
     <div>
         { children }
