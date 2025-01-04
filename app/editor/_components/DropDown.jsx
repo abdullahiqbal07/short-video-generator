e96e5909a659a84856d0da82da4df09221a54534
abdullah <abdullahiqbal599@gmail.com>
2025-01-04 23:50:39 +0500

    clean code

--- app/editor/_components/DropDown.jsx
+++ app/editor/_components/DropDown.jsx
@@ -11,7 +11,10 @@ function DropDown({ label, defaultValue, options, handleInputChange }) {
   return (
     <div className="mt-3 flex flex-col gap-2">
       <label>{label}</label>
-      <Select value={defaultValue} onValueChange={(value)=>handleInputChange(value)}>
+      <Select
+        value={defaultValue}
+        onValueChange={(value) => handleInputChange(value)}
+      >
         <SelectTrigger className="w-full bg-white">
           <SelectValue placeholder={defaultValue || "Select an option"} />
         </SelectTrigger>
